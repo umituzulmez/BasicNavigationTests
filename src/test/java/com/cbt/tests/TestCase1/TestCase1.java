a61a71e912a82acba1c7ef905554e3dfdde47364
Umit Uzulmez <umituzulmez@gmail.com>
2019-11-12 00:43:17 +0000

    Test1

---
+++ src/test/java/com/cbt/tests/TestCase1/TestCase1.java
@@ -0,0 +1,52 @@
+package com.cbt.tests.TestCase1;
+
+import com.cbt.utilities.BrowserFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.sql.SQLOutput;
+
+public class TestCase1 {
+
+    public static void main(String[] args) {
+
+        /**
+         * Go to ebay
+         * enter search term
+         * click on search button
+         * print number of results
+         */
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+
+        driver.get("https://www.ebay.com/");
+
+        WebElement searchBox = driver.findElement(By.id("gh-ac"));
+
+        searchBox.sendKeys("search");
+
+        WebElement searchButton = driver.findElement(By.id("gh-btn"));
+
+        searchButton.click();
+
+        WebElement searchResult = driver.findElement(By.className("srp-controls__count-heading"));
+
+        String searchResultText = searchResult.getText();
+
+        System.out.println(searchResultText);
+
+        String[] arr = searchResultText.split(" result");
+
+        String numberText = arr[0];
+
+        numberText = numberText.replace(",","");
+
+        int number = Integer.parseInt(numberText);
+
+        System.out.println("number of search result is " + number);
+
+        driver.quit();
+
+    }
+}
